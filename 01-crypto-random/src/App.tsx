8ccd3ae90cec6bc2309f914b151eb909c7c61e84
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2026-02-19 12:29:11 -0700

    use Query

--- 01-crypto-random/src/App.tsx
+++ 01-crypto-random/src/App.tsx
@@ -1,17 +1,49 @@
-import { useState } from "react";
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
 
-// http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1
+interface  DataAPI{
+  answer:string,
+  forced:Boolean,
+  image:string
+}
+
+
+const getCryptoNumber = async (): Promise<DataAPI> => {
+  const resp = await fetch(
+    "https://yesno.wtf/api",
+  ).then((resp) => resp.json());
+  console.log("resp", resp);
+  return resp;
+};
 function App() {
+  const {
+    isLoading,
+    isError,
+    data,
+    error,
+    refetch,
+    isFetching
+  } = useQuery({
+    queryKey: ["randomNumber"],
+    queryFn: getCryptoNumber,
+  });
 
-  
-
-  const [number, setNumber] = useState();
+  if (isLoading || isFetching) {
+    return <h1>Cargando</h1>;
+  }
+  if (isError) {
+    return <h1>Ocurrio un error::{error.message}</h1>;
+  }
 
   return (
     <>
       <h1>Hola Mundo</h1>
-      <h3>{number}</h3>
+      <h3>answer: {data?.answer}</h3>
+      <h3>{`forced: ${data?.forced ? 'NO' : 'NO'}`}</h3>
+      <img src={data?.image ? data.image : " " }/>
+      <button onClick={()=>refetch()}>
+        Refrescar
+      </button>
     </>
   );
 }
